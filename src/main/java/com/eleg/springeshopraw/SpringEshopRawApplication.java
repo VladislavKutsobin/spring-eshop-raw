1bc61db6815029b8fa86d98421a82a267d9c8476
VladislavKutsobin <v.kutsobin@gms-worldwide.com>
2022-09-28 15:00:21 +0300

    initial commit

---
+++ src/main/java/com/eleg/springeshopraw/SpringEshopRawApplication.java
@@ -0,0 +1,13 @@
+package com.eleg.springeshopraw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEshopRawApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEshopRawApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eleg/springeshopraw/SpringEshopRawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eleg.springeshopraw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEshopRawApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
